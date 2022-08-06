5d68e4a24a17492cae092542881fd010c84c9e95
dugslade <dugslade@us.ibm.com>
2022-08-06 18:58:17 -0400

    Update ResourceLinks.js

--- src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
+++ src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -16,7 +16,11 @@ const links = [
     href: 'https://www.ibm.com/cloud/aiops',
   },
   {
-    title: 'PTEG - Partner Enablement',
+    title: 'Instana PTEG - Tech Guide',
+    href: 'https://ibm.seismic.com/Link/Content/DCqJR68b93JQRG7WcMHQ39hBT3p3',
+  },
+  {
+    title: 'Top PTEG - Partner Enablement',
     href: 'https://ibm.seismic.com/Link/Content/DCGdFfFmRb3Rb8TRTHhHPXjbCDWj',
   },
 ];
