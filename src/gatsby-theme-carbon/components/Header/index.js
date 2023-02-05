6990fd2fa686f9a82ec6b5fb72483e1720451237
Doug Slade <dugslade@us.ibm.com>
2023-02-04 19:53:46 -0500

    Title change - technical

--- src/gatsby-theme-carbon/components/Header/index.js
+++ src/gatsby-theme-carbon/components/Header/index.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = (props) => (
   <Header {...props}>
-    &nbsp;<span>Instana Partner 1-Day Workshop</span>
+    &nbsp;<span>Instana Partner 1-Day Technical Workshop</span>
   </Header>
 );
 
